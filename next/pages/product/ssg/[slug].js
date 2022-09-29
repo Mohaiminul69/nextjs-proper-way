93425f51b1a204a231d2c4c9a35b4575dfb7a3e0
mohaiminul69 <mohaiminul69@gmail.com>
2022-09-29 11:42:40 +0600

    ssg implemented

---
+++ next/pages/product/ssg/[slug].js
@@ -0,0 +1,33 @@
+import React from "react";
+import { sanityClient } from "../../../utils/sanity/client";
+
+const Product = ({ product }) => {
+  return (
+    <div>
+      <p>{product.title}</p>
+    </div>
+  );
+};
+
+export default Product;
+
+export const getStaticProps = async (context) => {
+  const { slug } = context.params;
+  const product = await sanityClient.fetch(
+    '*[_type=="product" && defined(slug.current) && !(_id in path("drafts.**")) && slug.current == $slug][0]{..., "slug":slug.current}',
+    { slug }
+  );
+  return {
+    props: { product },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const paths = await sanityClient.fetch(
+    '*[_type=="product" && defined(slug.current) && !(_id in path("drafts.**"))]{"params":{"slug":slug.current}}'
+  );
+  return {
+    paths,
+    fallback: false,
+  };
+};
